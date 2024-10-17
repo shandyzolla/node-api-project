65e2cc1f6766d09b36e5ef279a7568b94ecbc087
Shandy Zolla <46232418+shandyzolla@users.noreply.github.com>
2024-10-17 20:28:42 +0700

    Delete frontend directory

--- frontend/src/services/api.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-const API_URL = 'http://localhost:5000';
-
-export const getItems = () => axios.get(`${API_URL}/items`);
-export const addItem = (item) => axios.post(`${API_URL}/items`, item);
-export const UpdateItem = (id, item) => axios.put(`${API_URL}/items/${id}`, item);
-export const deleteItem = (id) => axios.delete(`${API_URL}/items/${id}`);
